2d7de66f66f88a11c97e5ef9500fbee47bf0dcb5
Kajetan Lewkowicz <kajetan.lewkowicz@housecallpro.com>
2025-02-09 16:15:05 +0100

    Enable copilot and lsp completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,15 +10,24 @@ return {
       },
     },
     {
+      "zbirenbaum/copilot-cmp",
+      dependencies = { "zbirenbaum/copilot.lua" },
+      config = function()
+        require("copilot_cmp").setup()
+      end
+    },
+    {
       "hrsh7th/nvim-cmp",
       config = function()
         local cmp = require("cmp")
+        local has_words_before = function()
+          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+        end
         require("luasnip.loaders.from_vscode").lazy_load()
   
         cmp.setup({
-          completion = {
-            autocomplete = false
-          },
           snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
@@ -34,8 +43,16 @@ return {
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<Tab>"] = vim.schedule_wrap(function(fallback)
+              if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              else
+                fallback()
+              end
+            end),
           }),
           sources = cmp.config.sources({
+            { name = "copilot" },
             { name = "nvim_lsp" },
             { name = "luasnip" },
           }, {
