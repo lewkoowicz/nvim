4a77ce30fb6575367977a2b90a4fa9b0241c5887
Kajetan Lewkowicz <kajetan.lewkowicz@housecallpro.com>
2025-02-09 21:32:54 +0100

    Remove unnecessary copilot dependency

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -11,7 +11,6 @@ return {
     },
     {
       "zbirenbaum/copilot-cmp",
-      dependencies = { "zbirenbaum/copilot.lua" },
       config = function()
         require("copilot_cmp").setup()
       end
