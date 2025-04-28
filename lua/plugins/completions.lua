aff31fd7aa96ad57a951d2254a65291f3116d806
Kajetan Lewkowicz <kajetan.lewkowicz@housecallpro.com>
2025-04-28 17:16:33 +0200

    Revert "Revert "Revert "Disable copilot, completions and lsp"""
    This reverts commit b640aa53ba6f37b4c2dbe300e5d62f63611f5360.

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,63 @@
+return {
+    {
+      "hrsh7th/cmp-nvim-lsp"
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    {
+      "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end
+    },
+    {
+      "hrsh7th/nvim-cmp",
+      config = function()
+        local cmp = require("cmp")
+        local has_words_before = function()
+          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+          return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+        end
+        require("luasnip.loaders.from_vscode").lazy_load()
+  
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+          window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            ["<Tab>"] = vim.schedule_wrap(function(fallback)
+              if cmp.visible() and has_words_before() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              else
+                fallback()
+              end
+            end),
+          }),
+          sources = cmp.config.sources({
+            { name = "copilot" },
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+          }, {
+            { name = "buffer" },
+          }),
+        })
+      end,
+    },
+  }
