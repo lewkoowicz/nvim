60540366b350e61f8f83f26faafd700c4ba9ce93
Kajetan Lewkowicz <kajetan.lewkowicz@housecallpro.com>
2025-04-29 09:25:44 +0200

    Disable copilot

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -9,24 +9,27 @@ return {
         "rafamadriz/friendly-snippets",
       },
     },
-    {
-      "zbirenbaum/copilot-cmp",
-      config = function()
-        require("copilot_cmp").setup()
-      end
-    },
+    -- {
+    --   "zbirenbaum/copilot-cmp",
+    --   config = function()
+    --     require("copilot_cmp").setup()
+    --   end
+    -- },
     {
       "hrsh7th/nvim-cmp",
       config = function()
         local cmp = require("cmp")
-        local has_words_before = function()
-          if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-          return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
-        end
+        -- local has_words_before = function()
+        --   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+        --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        --   return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+        -- end
         require("luasnip.loaders.from_vscode").lazy_load()
   
         cmp.setup({
+          completion = {
+            autocomplete = false
+          },
           snippet = {
             expand = function(args)
               require("luasnip").lsp_expand(args.body)
@@ -41,17 +44,17 @@ return {
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            ["<Tab>"] = vim.schedule_wrap(function(fallback)
-              if cmp.visible() and has_words_before() then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-              else
-                fallback()
-              end
-            end),
+            ["<CR>"] = cmp.mapping.confirm({ select = true })
+            -- ["<Tab>"] = vim.schedule_wrap(function(fallback)
+            --   if cmp.visible() and has_words_before() then
+            --     cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            --   else
+            --     fallback()
+            --   end
+            -- end),
           }),
           sources = cmp.config.sources({
-            { name = "copilot" },
+            -- { name = "copilot" },
             { name = "nvim_lsp" },
             { name = "luasnip" },
           }, {
